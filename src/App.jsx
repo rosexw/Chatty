50209eab93ab8ad0d4f024141964d0228c8bf192
Rose Wan <rose.wan@me.com>
2017-06-27 10:41:04 -0400

    day one

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MessageList from './MessageList.jsx';
 
 class App extends Component {
   render() {
+    console.log("Rendering <App/>");
     return (
       <div>
         <nav className="navbar">
