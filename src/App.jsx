071b00cdfe67985cfedcf5875b9170c0e56d113a
Rose Wan <rose.wan@me.com>
2017-06-28 16:44:19 -0400

    server 3001 connected Websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,13 @@ class App extends Component {
   }
   componentDidMount() {
     console.log("componentDidMount <App />");
-    // setTimeout(() => {
-    //   console.log("Simulating incoming message");
-    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    //   const messages = this.state.messages.concat(newMessage)
-    //   this.setState({messages: messages})
-    // }, 3000);
+    this.socket = new WebSocket("ws://localhost:3001", "protocolOne");
+    this.socket.onopen = (event) => {
+      this.socket.send('Connected to server.');
+    }
+    this.socket.onmessage = (event) => {
+      console.log(event.data);
+    }
   }
   sendMessage = (message) => {
       const newMessage = {id: this.state.messages.length+1, username: this.state.currentUser.name, content: message};
